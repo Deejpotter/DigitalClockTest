d796607182f52c11d4a56de9a491a166763daa44
Daniel Potter <daniel.potter@onealphatech.com>
2022-07-23 19:46:45 +1000

    Added some structure

---
+++ settings/settings.jsx
@@ -0,0 +1,12 @@
+<>
+  < ColorSelect
+    settingsKey="color" />
+  <Section
+    description={<Text> Description <Link source="/">here</Link></Text>}
+    title={<Text bold align="center">Demo Settings</Text>}>
+    <Text>
+      This is a very basic demo settings page to show off some of the current
+      capabilities of the Companion Settings library.
+    </Text>
+  </Section>
+</>
